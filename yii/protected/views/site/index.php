ac945375b53ce673e044d3c27f38b1730fcd9222
fuel <gif@fuel-it-up.com>
2014-06-24 19:48:29 +0800

    update2

---
+++ yii/protected/views/site/index.php
@@ -0,0 +1,124 @@
+<div class="home" data-style="opacity:0;" data-animate="opacity:1;" data-delay="500" data-time="500">
+
+    <!--  -->
+    <div class="sec_home_head" data-style="opacity:0;" data-animate="opacity:1;" data-delay="800" data-time="500">
+        <div class="sec_home_head_con">雅漾携手中国医师协会皮肤科医师分会（CDA），正式启动雅漾“1000Families·湿疹儿童优享乐生活”关爱行动，为一千名湿疹儿童免费送上湿疹护理套装，携爱助力儿童家庭。</div>
+        <a class="sec_home_head_more" href="<?=Yii::app()->createUrl('/site/activity/')?>"></a>
+        <div class="sec_home_head_num"><?=$recordCount?></div>
+
+        <a href="<?=Yii::app()->createUrl('/information/update')?>" class="sec_home_head_btn1"  title="立即申请"></a>
+        <a href="http://service.weibo.com/share/share.php?title=%e4%bd%a0%e7%9f%a5%e9%81%93%e5%90%97%ef%bc%8c%e4%b8%8d%e6%98%af%e6%89%80%e6%9c%89%e7%9a%84%e7%97%92%e9%83%bd%e8%83%bd%e5%88%9b%e9%80%a0%e7%ac%91%e5%a3%b0http%3a%2f%2ft.cn%2fRvLmPUd+%e7%9c%8b%e4%ba%86%e8%a7%86%e9%a2%91%e6%89%8d%e7%9f%a5%e9%81%93%ef%bc%8c%e4%b8%ad%e5%9b%bd%e6%ad%a3%e6%9c%89%e6%95%b0%e7%99%be%e4%b8%87%e5%84%bf%e7%ab%a5%e6%ad%a3%e5%9c%a8%e5%bf%8d%e5%8f%97%e6%b9%bf%e7%96%b9%e6%89%80%e5%b8%a6%e6%9d%a5%e7%9a%84%e7%98%99%e7%97%92%e5%92%8c%e7%97%9b%e8%8b%a6%ef%bc%8c%e4%bb%96%e4%bb%ac%e9%9c%80%e8%a6%81%e4%bd%a0%e7%9a%84%e5%85%b3%e6%80%80%e4%b8%8e%e6%94%af%e6%8c%81%ef%bc%81%e9%9b%85%e6%bc%be%e6%90%ba%e6%89%8bCDA%ef%bc%8c%e5%8d%b3%e5%b0%86%e5%90%af%e5%8a%a8%231000Families%23%e8%a1%8c%e5%8a%a8%ef%bc%8c%e8%ae%a9%e6%b9%bf%e7%96%b9%e5%84%bf%e7%ab%a5%e4%bc%98%e4%ba%ab%e4%b9%90%e7%94%9f%e6%b4%bb%e3%80%82%e7%ab%8b%e5%8d%b3%e7%99%bb%e5%bd%95%e7%bd%91%e7%ab%99%ef%bc%9ahttp%3a%2f%2ft.cn%2fRvwx3b6&amp;pic=http://www.eau-thermale-avene.cn/eczemakids/weibo-banner.jpg&amp;appkey=4266983005" target="_blank" class="sec_home_head_btn2"  title="分享到微博"></a>
+        <div class="sec_home_head_btns">
+            <div class="popshow_list"></div>
+            <div class="popshow_rule"></div>
+        </div>
+    </div>
+    <!--  -->
+    <!--  -->
+    <div class="section sec_home2 cs-clear">
+        <div class="home_carefor home_infor" data-style="opacity:0;" data-animate="opacity:1;" data-delay="1100" data-time="500">
+            <h2><a href="<?=Yii::app()->createUrl('/care/')?>">雅漾关怀</a></h2>
+            <a href="<?=Yii::app()->createUrl('/care/')?>" class="home_more"></a>
+            <div class="home_infopop">
+                <div class="home_carefor_share"></div>
+                <div class="home_carefor_txt">为了每个孩子的清澈笑容，雅漾一直致力于特应性皮炎儿童的护理，并携手中国医师协会皮肤科医师分会，设立首家面向中国特应性患儿及家长的免费教育课堂“雅漾特应性皮炎之家”及国内首个特应性皮炎基金会“特应性皮炎基金会”。</div>
+            </div>
+        </div>
+        <div class="home_about home_infor" data-style="opacity:0;" data-animate="opacity:1;" data-delay="1300" data-time="500">
+            <h2><a href="<?=Yii::app()->createUrl('/about/')?>">关于湿疹</a></h2>
+            <a href="<?=Yii::app()->createUrl('/about/')?>" class="home_more"></a>
+            <div class="home_infopop">
+                <div class="home_about_share"></div>
+                <div class="home_about_txt">什么是湿疹？湿疹与特应性皮炎如何区分？如何诊断是否已患有湿疹？点击了解更多信息。</div>
+            </div>
+        </div>
+    </div>
+    <!--  -->
+    <div class="section sec_home3 home_baike home_infor" data-style="opacity:0;" data-animate="opacity:1;" data-delay="1400" data-time="500">
+        <h2><a href="<?=Yii::app()->createUrl('/guide/')?>">湿疹护理</a></h2>
+        <a href="<?=Yii::app()->createUrl('/guide/')?>" class="home_more"></a>
+        <div class="home_baike_title">护理<span>湿疹</span>不容忽视的<span>14</span>个问题，<br />简单易行的科学<span>护理方法</span>及权威<span>专家问答</span>。</div>
+        <div class="home_infopop">
+            <div class="home_baike_share"></div>
+            <div class="home_baike_txt">围绕十四个重要关键词，为父母提供常见问题回答与简单易行建议。</div>
+        </div>
+    </div>
+</div>
+<div class="popshare"></div>
+<div class="pop home_pop" id="pop_list">
+    <div class="popclose"></div>
+    <div class="home_popcom">
+        <h2>所有参与家庭</h2>
+        <div class="homepopfm_list cs-clear">
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+            <p class="cs-clear"><span>某某某</span><span>2岁</span><span>上海</span></p>
+        </div>
+    </div>
+</div>
+<div class="pop home_pop" id="pop_rule">
+    <div class="popclose"></div>
+    <div class="home_popcom">
+        <h2>活动规则</h2>
+        <div class="rule_wrap">
+            <p><strong>申请时间</strong><strong> </strong><br />
+                2014年7月1日-2014年8月31日<strong></strong></p>
+            <p><strong>申请方式 </strong><strong> </strong><br />
+                在线申请：在本活动官方网站利用邮箱或微博注册，登陆后请按照提示完成宝宝资料填写，制作宝宝电子档案，进入1000Families候选名单。</p>
+            <p><strong>申领名额及产品</strong><strong> </strong><br />
+                雅漾将在所有申请者中随机抽取1000个湿疹儿童家庭，免费赠送湿疹特效护理套装（内含6支三重修护滋润霜200ml，6瓶舒护活泉喷雾150ml，及湿疹护理教育视频、湿疹护理手册）。自2014年7月15日起，被选中的儿童名单将每两周在活动官网上陆续公布，请密切关注官网信息，届时雅漾也将及时与您取得联系。</p>
+            <p><strong>法律声明</strong><strong> </strong></p>
+            <p>本网站的雅漾&ldquo;1000 Families  湿疹儿童优享乐生活&rdquo;关爱行动（&ldquo;本次活动&rdquo;）适用雅漾中国网站<a href="http://www.eau-thermale-avene.cn/"><strong>http://www.eau-thermale-avene.cn/</strong></a>的法律声明和如下本次活动的法律声明。本网站用户和活动参与者应当认真阅读雅漾中国网站的法律声明和本次活动的法律声明。网站用户和活动参与者不同意雅漾中国网站的法律声明和本次活动的法律声明的，应当立即停止使用网站和在网站上注册。使用和在本网站上注册的行为视为同意雅漾中国网站的法律声明和本次活动的法律声明，以及同意遵守该等法律声明，并且受该等法律声明的约束。 <br />
+                如下本次活动的法律声明是雅漾中国网站法律声明的组成部分。若有冲突的，以如下法律声明为准。 <br />
+                为本活动的目的，雅漾中国网站的法律声明和本次活动的法律声明以下合称为&ldquo;法律声明&rdquo;。<strong> </strong></p>
+            <ul>
+                <li>网站使用者在网站上注册账户时，会被要求填写如下真实的个人基本信息：姓名、身份证信息、地址、电话、电子邮箱、手机号码等真实的个人信息（&ldquo;<strong>个人信息</strong>&rdquo;），和用户基本信息：用户名称、用户密码、用户头像等用户信息（&ldquo;<strong>用户信息</strong>&rdquo;）。网站使用者在成功注册后，可以在网站上上传其个人或其子女的相片、图像、录像或其他资料（&ldquo;<strong>上传资料</strong>&rdquo;）。 </li>
+                <li>网站使用者在网站上注册账户，和/或在网站上上传其个人或其子女资料的行为是其自愿的行为，并且明确表示其同意参与本次活动。在网站上注册账户，和/或在网站上提交其个人或其子女资料的该等网站使用者为本次活动的参与者（&ldquo;<strong>活动参与者</strong>&rdquo;）。</li>
+                <li>活动参与者在此承诺并保证其有权上传、使用和授权第三人使用其提交的上传资料，有权使用和授权第三人商业性对外公开使用该等上传资料，并且该等使用或授权不侵犯上传资料所有人或其他权利人的任何权利。 </li>
+                <li>活动参与者在此承诺并保证其有权使用相片肖像权人的肖像，并有权授权第三人公开商业性的使用该等相片肖像权人的肖像，并且不侵犯相片肖像权人的任何权利。 </li>
+                <li>活动参与者在此授权雅漾和本次活动相关的广告商在本次活动期间及本次活动结束后1年内，为本次活动及其相关产品的宣传中无偿使用活动参与者的上述用户信息和上传资料，并且无需另行征求活动参与者的同意或确认。 </li>
+                <li>活动参与者在此授权雅漾和本次活动相关的广告商在本次活动期间及本次活动结束后1年内，为本次活动及其相关产品的宣传中无偿公开商业性的使用上传资料中相片肖像权人的肖像。</li>
+                <li>商业宣传推广包含任何形式的商业活动（包括但不限于数字媒体、户外广告、平面广告、挂历或其他商业性展览等各种商业性活动）。 </li>
+                <li>活动参与者在此承诺并保证其将保护、赔偿并且使雅漾和相关广告商免受第三方因雅漾和相关广告商基于本次活动或活动结束后1年内因使用或进行的任何活动宣传和相关产品宣传而产生的针对雅漾和相关广告商因使用活动参与者提供的用户信息和/或上传资料所提出的任何诉讼、责任、损失、开支和花费赔偿要求。如若雅漾和相关广告商因使用活动参与者提供的用户信息和上传资料而遭受任何损失的，活动参与者应当赔偿雅漾和相关广告商的损失。</li>
+                <li>活动参与者被随机抽中，有权获得活动产品的，活动参与者获得活动产品的权利不得自行撤销、转让或出售。活动参与者在成功领取活动产品后，不得退换。 </li>
+                <li>如遇意外，雅漾无法提供指定活动产品的，雅漾有权以同等价值的产品代替，活动产品不得兑换现金或作价销售。 </li>
+                <li>如因活动参与者个人原因不能领取活动产品的，视为其自动放弃该权利，雅漾有权不对其进行任何形式的补偿或赔偿。 </li>
+                <li>如果发现活动参加者在本次活动中使用不正当的手段参与活动，雅漾有权在不事先通知的情况下取消其参加活动的资格。即使在活动参与者被随机抽中，并获得活动产品后，雅漾仍有权在不事先通知的情况下取消其参加活动的资格。雅漾有权不对其进行任何形式的补偿或赔偿。 </li>
+                <li>雅漾不对因网络传输原因而导致参加者提交的信息错误或延误担任何责任。 </li>
+                <li>雅漾、关联公司、广告公司、网络合作伙伴的员工及其家属不得参与此次活动，以示公允。 </li>
+                <li>如遇不可抗力或政府管制原因，雅漾有权取消本次活动。雅漾有权不对活动参与者进行任何形式的赔偿。 </li>
+                <li>本活动最终解释权归雅漾及皮尔法伯中国所有。 </li>
+            </ul>
+            <br clear="all" />
+            <p>&nbsp;</p>
+            <p>申请步骤-隐私条款<br />
+                本网站的雅漾&ldquo;1000 Families  湿疹儿童优享乐生活&rdquo;关爱行动（&ldquo;本次活动&rdquo;）适用雅漾中国网站<a href="http://www.eau-thermale-avene.cn/"><strong>http://www.eau-thermale-avene.cn/</strong></a>的隐私条款和如下本次活动的隐私条款。本网站用户和活动参与者应当认真阅读雅漾中国网站的隐私条款和本次活动的隐私条款。网站用户和活动参与者不同意雅漾中国网站的隐私条款和本次活动的隐私条款的，应当立即停止使用网站和在网站上注册。使用和在本网站上注册的行为视为同意雅漾中国网站的隐私条款和本次活动的隐私条款，以及同意遵守该等隐私条款，并且受该等隐私条款的约束。 <br />
+                如下本次活动的隐私条款是雅漾中国网站隐私条款的组成部分。若有冲突的，以如下隐私条款为准。 <br />
+                为本活动的目的，雅漾中国网站的隐私条款和本次活动的隐私条款以下合称为&ldquo;隐私条款&rdquo;。<strong> </strong></p>
+            <ul>
+                <li>网站使用者在网站上注册账户时，会被要求填写如下真实的个人基本信息：姓名、身份证信息、地址、电话、电子邮箱、手机号码等的真实的个人信息（&ldquo;<strong>个人信息</strong>&rdquo;），和用户基本信息：用户名称、用户密码、用户头像等用户信息（&ldquo;<strong>用户信息</strong>&rdquo;）。网站使用者在成功注册后，可以在网站上上传其个人或其子女的相片、图像、录像或其他资料（&ldquo;<strong>上传资料</strong>&rdquo;）。 </li>
+                <li>网站使用者在网站上注册账户，和/或在网站上上传提交其个人或其子女资料的行为是其自愿的行为，并且明确表示其同意参与本次活动。在网站上注册账户，和/或在网站上提交其个人或其子女资料的该等网站使用者为本次活动的参与者（&ldquo;<strong>活动参与者</strong>&rdquo;）。</li>
+                <li>雅漾及其活动运营方承诺将为实现本次活动的目的合理使用活动参与者的个人信息。并且除非根据法律或政府的强制性规定，在未得到活动参与者的许可之前，雅漾及活动运营方承诺不将活动参与者的个人信息披露给无关的第三方。 </li>
+                <li>在本次活动期间及本次活动结束后1年内，雅漾对活动参与者提供的用户信息和上传资料将公开用于本次活动及相关产品的商业宣传推广。</li>
+                <li>活动参与者承诺并保证其在使用本网站过程中应当遵守中华人民共和国的相关法律法规，不进行任何涉嫌非法的行为或任何可能造成雅漾财产或名誉损失的行为。对于活动参与者违反法律法规的行为，由活动参与者自行承担法律相关责任，与雅漾和皮尔法伯中国无关。 </li>
+            </ul>
+        </div>
+    </div>
+</div>
